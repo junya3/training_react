16cf1368a5d4adf33d155cc30e99ba0f78390303
junya03 <barao.0728@gmail.com>
2025-05-02 17:27:28 +0900

    baseのurlを変更しました。

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: '/04',  // サブディレクトリ名（例: /04）
+  assetPrefix: '/04', // CSS、JSなど静的ファイルのパスを設定
 };
 
 module.exports = nextConfig;
