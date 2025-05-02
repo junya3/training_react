9650e20044343587d5f5854859ebad3cfe22d920
junya03 <barao.0728@gmail.com>
2025-05-02 12:34:23 +0900

    newsSec add

---
+++ src/components/parts/CardGrid2.tsx
@@ -0,0 +1,24 @@
+import styles from "./CardGrid2.module.css";
+type Content = {
+    title: string,
+    date?: string,
+    content: string[],
+}
+
+const CardGrid2 = ({content}: {content:Content[]}) => {
+    return (
+        <>
+            <ul className={styles.CardGrid2}>
+                {content.map((item) => (
+                    <li>
+                        <p><time>{item.date}</time></p>
+                        <h3>{item.title}</h3>
+                        <p>{item.content}</p>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
+}
+
+export default CardGrid2;
--- src/components/section/News.tsx
+++ src/components/section/News.tsx
@@ -1,9 +1,20 @@
+import CardGrid2 from "@/components/parts/CardGrid2";
+import CenterTitle from "@/components/parts/CenterTitle";
+import news from "@/data/news";
+import styles from "./News.module.css";
+
+const title = "お知らせ";
+const subTitle = "最新情報";
+
 const News = () => {
     return (
         <>
-            
+            <section className={styles.News}>
+                <CenterTitle title={title} subTitle={subTitle}/>
+                <CardGrid2 content={news}/>
+            </section>
         </>
     )
 }
 
-export default News ;
+export default News;
