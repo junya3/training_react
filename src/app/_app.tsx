6656fe59cc08068b3c5e3efc054dcddc802dff2c
junya03 <barao.0728@gmail.com>
2025-05-02 17:02:41 +0900

    復帰

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,13 +1,6 @@
-// フォントの読み取り
-import { M_PLUS_1 } from "next/font/google";
+
 import "@assets/css/reset.css";
 import "@assets/css/style.css";
-
-const mPlus1 = M_PLUS_1({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], // 使用したい太さを指定
-  subsets: ["latin"], // サブセット（必要なら "latin-ext" や "japanese" など）
-  display: "swap", // 推奨設定
-});
 
 import type { AppProps } from "next/app";
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,10 @@ import "@assets/css/style.css";
 
 import Keyvisual from "@/components/section/Keyvisual";
 import Concept from "@/components/section/Concept";
+import Point from "@/components/section/Point";
+import BreadSec from "@/components/section/TopBread";
+import TopCafe from "@/components/section/TopCafe";
+import News from "@/components/section/News";
 
 
 
@@ -12,6 +16,13 @@ export default function Home() {
     <>
         <Layout>
             <Keyvisual/>
+            <div className="container">
+              <Concept/>
+              <Point/>
+              <BreadSec/>
+              <TopCafe />
+              <News/>
+            </div>
         </Layout>
     </>
   );
