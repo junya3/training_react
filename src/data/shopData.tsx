21f65f4d9d2c9b2b5676f80c17f0ac7ae3ea8dcf
junya03 <barao.0728@gmail.com>
2025-05-02 18:03:17 +0900

    add shopData

---
+++ src/data/shopData.tsx
@@ -0,0 +1,26 @@
+const shopData = {
+    shop: {
+        name: "Bakery NIKORI",
+        concept: [
+            "にこりと笑顔になる、やさしいパンを。",
+            "毎日の食卓に、ちょっとした幸せを。"
+        ],
+    },
+    business: {
+        time :  {
+            mon: "8:00 - 19:00",
+            tue: "8:00 - 19:00",
+            wed: "8:00 - 19:00",
+            thu: "8:00 - 19:00",
+            fri: "8:00 - 19:00",
+            sat: "7:30 - 18:00",
+            sun: "7:30 - 18:00"
+          },
+        tel: "03-1234-5678",
+        mail: "info@example.com",
+    },
+    access : {
+        address: "〒150-0001 東京都渋谷区神宮前X-X-X",
+        direction: "表参道駅 A2出口から徒歩5分",
+    }
+}
