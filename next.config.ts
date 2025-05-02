196c80157f88825d39d0c482b399e642a623773d
junya03 <barao.0728@gmail.com>
2025-05-02 17:36:23 +0900

    nextconfigについての記述を行いました。

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@
 const nextConfig = {
   output: "export",
   distDir: "out",
-  basePath: '/04',  // サブディレクトリ名（例: /04）
-  assetPrefix: '/04', // CSS、JSなど静的ファイルのパスを設定
+  // 基本的には下記２点をコメントアウト、実装時は解除
+  // basePath: '/04',  // サブディレクトリ名（例: /04）
+  // assetPrefix: '/04', // CSS、JSなど静的ファイルのパスを設定
   images: {
     unoptimized: true,
   },
