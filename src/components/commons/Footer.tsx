d9cbd12f7fb1da369d7c72491ab794aa326f1b56
小原淳也 <oharaatsuya@usernomacbook.local>
2025-05-03 11:44:29 +0900

    add Footer

--- src/components/commons/Footer.tsx
+++ src/components/commons/Footer.tsx
@@ -8,36 +8,45 @@ const Footer = () => {
     return (
         <>
             <footer className={styles.Footer}>
-                <hgroup>
-                    <h1>{shop.name}</h1>
-                    <p>
-                        {shop.concept.map((span) => (
-                            <span>{span}</span>
-                        ))}
-                    </p>
-                </hgroup>
-                <div className={styles.business}>
-                    <p>営業情報</p>
-                    <div className={styles.b_text}>
-                        <p>営業時間</p>
-                        <ul>
-                        {shopData.business.time.map(({ day, label, time }) => (
-                            <li key={day}>
-                                <time itemProp="openingHours" dateTime={`${day} ${time}`}>
-                                    {label} {time.replace("-", " - ")}
-                                </time>
-                            </li>
+                <div className={styles.FooterInner}>
+                    <hgroup>
+                        <h1 className={styles.title}>
+                            <a href="/">
+                                {shop.name}
+                            </a>
+                        </h1>
+                        <p>
+                            {shop.concept.map((span, index) => (
+                                <span key={index}>{span}</span>
                             ))}
-                        </ul>
+                        </p>
+                    </hgroup>
+                    <div className={styles.business}>
+                        <p className={styles.title}>営業情報</p>
+                        <div>
+                            <p>営業時間</p>
+                            <ul>
+                            {business.time.map(({ day, label, time }) => (
+                                <li key={day}>
+                                    <time itemProp="openingHours" dateTime={`${day} ${time}`}>
+                                        {label} {time.replace("-", " - ")}
+                                    </time>
+                                </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={styles.access}>
+                        <p className={styles.title}>アクセス</p>
+                        <address>
+                            <p>{access.address}</p>
+                            <p>{access.direction}</p>
+                        </address>
                     </div>
                 </div>
-                <div className={styles.access}>
-                    <p>アクセス</p>
-                    <div>
-                        <p>{access.address}</p>
-                        <p>{access.direction}</p>
-                    </div>
-                </div>
+                <small>
+                    <p>&copy;2025 BAKERY NICORI All Rights Reserved.</p>
+                </small>
             </footer>
         </>
     )
--- src/data/shopData.tsx
+++ src/data/shopData.tsx
@@ -8,13 +8,8 @@ const shopData = {
     },
     business: {
         time: [
-            { day: "Mo", label: "月曜", time: "08:00-19:00" },
-            { day: "Tu", label: "火曜", time: "08:00-19:00" },
-            { day: "We", label: "水曜", time: "08:00-19:00" },
-            { day: "Th", label: "木曜", time: "08:00-19:00" },
-            { day: "Fr", label: "金曜", time: "08:00-19:00" },
-            { day: "Sa", label: "土曜", time: "07:30-18:00" },
-            { day: "Su", label: "日曜", time: "07:30-18:00" },
+            { day: "Mo - Fr", label: "月〜金", time: "08:00-19:00" },
+            { day: "Sa - Su", label: "土日祝", time: "07:30-18:00" },
           ],          
         tel: "03-1234-5678",
         mail: "info@example.com",
