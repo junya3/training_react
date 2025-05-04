e9159d8c7de51e5a81480905b5338d9385ab3b33
junya03 <obara.junya0728@gmail.com>
2025-05-04 16:23:01 +0900

    Add files via upload

---
+++ src/data/drinkList.tsx
@@ -0,0 +1,62 @@
+const drinkList = [
+  {
+    category : "コーヒー",
+    items : [
+      {
+        name : "バッチブリュー",
+        price : "400",
+      },
+      {
+        name : "ハンドドリップ",
+        price : "600",
+      },
+      {
+        name : "エスプレッソ",
+        price : "550",
+      },
+      {
+        name : "カフェラテ",
+        price : "650",
+      },
+      {
+        name : "カフェモカ",
+        price : "700",
+      },
+      {
+        name : "水出しコーヒー",
+        price : "580",
+      },
+    ],
+  },
+  {
+    category : "その他",
+    items : [
+      {
+        name : "ダージリン",
+        price : "480",
+      },
+      {
+        name : "アールグレイ",
+        price : "480",
+      },
+      {
+        name : "有機ルイボスティー",
+        price : "520",
+      },
+      {
+        name : "抹茶ラテ",
+        price : "650",
+      },
+      {
+        name : "ブラッドオレンジジュース",
+        price : "500",
+      },
+      {
+        name : "リンゴジュース",
+        price : "480",
+      },
+    ],
+  },
+]
+
+export default drinkList;
