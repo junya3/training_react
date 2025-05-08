6e15d9550292b0d19df1c4a4df01d96a535aef40
junya03 <barao.0728@gmail.com>
2025-05-08 12:29:56 +0900

    configの訂正

--- next.config.ts
+++ next.config.ts
@@ -8,8 +8,8 @@ const nextConfig = {
   trailingSlash: true,
 
   // 基本的には下記２点をコメントアウト、実装時は解除
-  basePath: '/04',  // サブディレクトリ名（例: /04）
-  assetPrefix: '/04', // CSS、JSなど静的ファイルのパスを設定
+  // basePath: '/04',  // サブディレクトリ名（例: /04）
+  // assetPrefix: '/04', // CSS、JSなど静的ファイルのパスを設定
   images: {
     unoptimized: true,
   },
